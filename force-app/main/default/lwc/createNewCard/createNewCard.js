9bdf8e2970b041bd586d6e555e652633e688a454
yanunes11 <yannunes112@gmail.com>
2024-04-15 16:27:30 -0300

    feat: created temporary Static Resource image until AWS connection.

--- force-app/main/default/lwc/createNewCard/createNewCard.js
+++ force-app/main/default/lwc/createNewCard/createNewCard.js
@@ -1,4 +1,5 @@
 import { LightningElement, api } from 'lwc';
+import IMAGES from "@salesforce/resourceUrl/AdoptDogLogo";
 
 export default class CreateNewAdoptionCard extends LightningElement {
     @api iconName = "";
@@ -8,6 +9,8 @@ export default class CreateNewAdoptionCard extends LightningElement {
     @api objectApiName = "";
     @api objectFields = [];
     showFooter = false;
+    // YNASC DELETE AFTER IMPLEMENTED AWS
+    animalImage = IMAGES; 
     handleClick(event) {
         console.log(JSON.stringify(event));
     }
