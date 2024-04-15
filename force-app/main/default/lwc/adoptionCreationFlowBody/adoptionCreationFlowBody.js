d570f0417e010f6f4b3bc7a09f711ba2b216e161
yanunes11 <yannunes112@gmail.com>
2024-04-15 16:59:20 -0300

    feat: create adopter card and its connection to APex DB

--- force-app/main/default/lwc/adoptionCreationFlowBody/adoptionCreationFlowBody.js
+++ force-app/main/default/lwc/adoptionCreationFlowBody/adoptionCreationFlowBody.js
@@ -1,13 +1,19 @@
 import { LightningElement, api } from 'lwc';
 import animalInformation from '@salesforce/apex/AnimalQueries.getAnimalInformationById';
+import adopterInformation from '@salesforce/apex/AdopterQueries.getAdopterInformationById';
 
 export default class AdoptionCreationFlowBody extends LightningElement {
     animalId = 'a00aj000006C4GMAA0';
+    adopterId = 'a03aj000001LRskAAG';
     @api animal;
     animalObjectFields = [];
+    adopterObjectFields = [];
     error;
     connectedCallback() {
+
+        //YNASC Delete these functions. It should be invoked by the choosing the animal and adopter in the list
         this.getAnimalInformation(this.animalId);
+        this.getAdopterInformation(this.adopterId);
     }
 
     getAnimalInformation(animalId) {
@@ -22,12 +28,28 @@ export default class AdoptionCreationFlowBody extends LightningElement {
                 }
             }
             this.animalObjectFields = animal;
-            console.log('YNASC here: '+JSON.stringify(this.animalObjectFields));
         })
         .catch((error) => {
             console.log('YNASC error: '+JSON.stringify(error));
         })
-        console.log('YNASC final: '+JSON.stringify(this.animalObjectFields));
+    }
+
+    getAdopterInformation(adopterId) {        let adopter = [];
+        adopterInformation({adopterId: adopterId})
+        .then((result) => {
+
+            for (let key in result) {
+                if (key !== 'Id') {
+                    let cleanKey = key.replace("__c", "").replace("_", " ");
+                    let adopterObject = {'fieldName': cleanKey, 'value': result[key]};
+                    adopter.push(adopterObject);
+                }
+            }
+            this.adopterObjectFields = adopter;
+        })
+        .catch((error) => {
+            console.log('YNASC error: '+JSON.stringify(error));
+        })
     }
 
 }
