9f166ded602eafde8581b13580208dec73d33f6f
yanunes11 <yannunes112@gmail.com>
2024-04-29 09:29:25 -0300

    feat: send information from parent to child on the CreateAdoptionModal

--- force-app/main/default/lwc/createAdoptionModal/createAdoptionModal.js
+++ force-app/main/default/lwc/createAdoptionModal/createAdoptionModal.js
@@ -2,6 +2,18 @@ import { LightningElement, api } from 'lwc';
 export default class CreateAdoptionModal extends LightningElement {
     //Boolean tracked variable to indicate if modal is open or not default value is false as modal is closed when page is loaded 
     @api isModalOpen = false;
+    @api animalId = '';
+    @api adopterId = '';
+    @api animalFields = {};
+    @api adopterFields = {};
+
+    connectedCallback() {
+        console.log('YNASC animalId: '+JSON.stringify(this.animalId));
+        console.log('YNASC adopterId: '+JSON.stringify(this.adopterId));
+        console.log('YNASC animalFields: '+JSON.stringify(this.animalFields.fieldName));
+        console.log('YNASC animalFields: '+JSON.stringify(this.animalFields[0].fieldName));
+        console.log('YNASC adopterFields: '+JSON.stringify(this.adopterFields));
+    }
     openModal() {
         // to open modal set isModalOpen tarck value as true
         this.isModalOpen = true;
